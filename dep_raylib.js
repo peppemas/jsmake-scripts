49fa5461eb8b20750f35297384bc918acb517421
peppemas <giuseppe.mastrangelo@outlook.com>
2022-10-09 09:07:19 +0200

    added raylib

---
+++ dep_raylib.js
@@ -0,0 +1,25 @@
+function raylib() {
+    Log.info("### Compile RAYLIB");
+
+    var GITHUB_URL = "https://github.com/raysan5/raylib.git";
+    var VERSION = "4.2.0";
+
+    var DIR = INSTALL_LIB_DIR + "/raylib";
+
+    GitCloneIfNotExists(GITHUB_URL, VERSION, DIR);
+
+    var DPARAMS = [
+        "-D",PLATFORM,
+    ];
+    var INCLUDES = [
+        "-I",INSTALL_LIB_DIR + "/include",
+    ];
+    var SOURCES = [];
+
+    AMALGAMATED_SOURCES.push(SOURCES);
+    AMALGAMATED_DPARAMS.push(DPARAMS);
+    AMALGAMATED_INCLUDES.push(INCLUDES);
+    if (AMALGAMATED_INCLUDES_ONLY) return 0;
+
+    return 0;
+}
