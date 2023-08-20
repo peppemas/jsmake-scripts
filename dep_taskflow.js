e17df66e87120505445718766f7303a36a83c83b
peppemas <giuseppe.mastrangelo@outlook.com>
2023-08-20 09:08:16 +0200

    working on Vulkan Engine

--- dep_taskflow.js
+++ dep_taskflow.js
@@ -1,1 +1,20 @@
+// [X] Windows [] Linux [] OSX [] Android [] UWP [] WEB
+function taskflow() {
+    Log.info("### Compile TASKFLOW (Header Only)");
 
+    var GITHUB_URL = "https://github.com/taskflow/taskflow.git";
+    var VERSION = "v3.6.0";
+
+    var DIR = INSTALL_LIB_DIR + "/taskflow";
+
+    GitCloneIfNotExists(GITHUB_URL, VERSION, DIR);
+
+    var DPARAMS = [];
+    var INCLUDES = [
+        "-I", DIR
+    ];
+
+    AMALGAMATED_INCLUDES.push(INCLUDES);
+
+    return true;
+}
