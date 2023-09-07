47dd56a1a1c2371423270431dd71a37ab691cb91
peppemas <giuseppe.mastrangelo@outlook.com>
2023-09-07 14:57:22 +0200

    added fontstash library

---
+++ dep_fontstash.js
@@ -0,0 +1,24 @@
+function fontstash() {
+
+    Log.info("### Compile FONTSTASH (header only)");
+
+    var GITHUB_URL = "https://github.com/memononen/fontstash.git";
+    var VERSION = "master";
+
+    var DIR = INSTALL_LIB_DIR + "/fontstash";
+
+    GitCloneIfNotExists(GITHUB_URL, VERSION, DIR);
+
+    var SOURCES = [];
+    var DPARAMS = [];
+    var INCLUDES = [
+        "-I", DIR + "/src"
+    ];
+
+    AMALGAMATED_SOURCES.push(SOURCES);
+    AMALGAMATED_DPARAMS.push(DPARAMS);
+    AMALGAMATED_INCLUDES.push(INCLUDES);
+    if (AMALGAMATED_INCLUDES_ONLY) return 0;
+
+    return 0;
+}
