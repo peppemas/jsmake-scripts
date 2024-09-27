c88f1faefafd80dcbb7582cd485daa06ba024d01
peppemas <giuseppe.mastrangelo@gmail.com>
2024-09-27 16:05:56 +0200

    integration of soloud sound library as steroid

--- dep_soloud.js
+++ dep_soloud.js
@@ -18,13 +18,32 @@ function soloud()
     var SOURCES = [];
     var SOURCES_CORE = Directory.collectFilesWithExt(DIR + "/src/core", ".cpp", true, false);
     var SOURCES_FILTERS = Directory.collectFilesWithExt(DIR + "/src/filter", ".cpp", true, false);
-    var SOURCES_AUDIO = Directory.collectFilesWithExt(DIR + "/src/audiosource/wav", ".cpp", true, false);
-    SOURCES_AUDIO = SOURCES_AUDIO.concat(Directory.collectFilesWithExt(DIR + "/src/audiosource/wav", ".c", true, false));
-    var SOURCE_BACKEND = DIR + "/src/backend/miniaudio/soloud_miniaudio.cpp";
+    var SOURCES_WAV = Directory.collectFilesWithExt(DIR + "/src/audiosource/wav", ".cpp", true, false);
+    SOURCES_WAV = SOURCES_WAV.concat(Directory.collectFilesWithExt(DIR + "/src/audiosource/wav", ".c", true, false));
+    var SOURCES_AY = Directory.collectFilesWithExt(DIR + "/src/audiosource/ay", ".cpp", true, false);
+    var SOURCES_NOISE = Directory.collectFilesWithExt(DIR + "/src/audiosource/noise", ".cpp", true, false);
+    var SOURCES_OPENMPT = Directory.collectFilesWithExt(DIR + "/src/audiosource/openmpt", ".cpp", true, false);
+    var SOURCES_SFXR = Directory.collectFilesWithExt(DIR + "/src/audiosource/sfxr", ".cpp", true, false);
+    var SOURCES_SPEECH = Directory.collectFilesWithExt(DIR + "/src/audiosource/speech", ".cpp", true, false);
+    var SOURCES_TEDSID = Directory.collectFilesWithExt(DIR + "/src/audiosource/tedsid", ".cpp", true, false);
+
+    var SOURCES_MONOTONE = Directory.collectFilesWithExt(DIR + "/src/audiosource/monotone", ".cpp", true, false);
+    var SOURCES_BACKEND = DIR + "/src/backend/miniaudio/soloud_miniaudio.cpp";
+
     var DPARAMS = arrayToString(["-D",PLATFORM,"-D","WITH_MINIAUDIO"]);
 
-    SOURCES = SOURCES.concat(SOURCES_CORE, SOURCES_FILTERS, SOURCES_AUDIO, SOURCE_BACKEND);
-    SOURCES.push(SOURCE_BACKEND);
+    SOURCES = SOURCES.concat(
+        SOURCES_CORE,
+        SOURCES_FILTERS,
+        SOURCES_WAV,
+        SOURCES_AY,
+        SOURCES_MONOTONE,
+        SOURCES_NOISE,
+        SOURCES_OPENMPT,
+        SOURCES_SFXR,
+        SOURCES_SPEECH,
+        SOURCES_TEDSID,
+        SOURCES_BACKEND);
 
     AMALGAMATED_SOURCES.push(SOURCES);
     AMALGAMATED_DPARAMS.push(DPARAMS);
