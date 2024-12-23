6442ec1e50839f5c970a3e87568d332fedaf13c7
peppemas <giuseppe.mastrangelo@outlook.com>
2024-12-23 03:43:12 +0100

    added tootchain creation template

--- tools_cmake.js
+++ tools_cmake.js
@@ -41,3 +41,40 @@ function generateCMakeLists(
     }
 }
 
+function generateCMakeToolChain(
+    filename,
+    projectName,
+    isStaticLibrary,
+    arrSubdirs,
+    arrHeaders,
+    arrSources,
+    cflags,
+    arrDparams
+) {
+
+    var template_vars = {
+        "project_file": filename,
+        "project_name": projectName,
+        "creation_date": Date(),
+        "release": TARGET_BUILD === T_TARGET_BUILD.RELEASE,
+        "static_library": isStaticLibrary,
+        "array_subdirs": arrSubdirs,
+        "array_headers": arrHeaders,
+        "array_sources": arrSources,
+        "cflags": cflags,
+        "dparams": arrayToString(arrDparams)
+    };
+
+    var TEMPLATE_DIR = "./jsmake-scripts/cmake_templates";
+    var TEMPLATE_NAME = "toolchain_library.template";
+    var TEMPLATE_FILE = TEMPLATE_DIR + "/" + TEMPLATE_NAME;
+
+    if (Directory.exists(TEMPLATE_FILE)) {
+        var template_content = Directory.readTextFile(TEMPLATE_FILE);
+        var template_result = Template.render(template_content, JSON.stringify(template_vars));
+        Directory.writeTextFile(filename,template_result);
+    } else {
+        Log.error("Cannot find CMakeLists.template");
+    }
+
+}
